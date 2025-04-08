83e9c9b10b7160ccf8f088f0d11d0a713177e073
Nitik Sharma <nitikwork2001@gmail.com>
2025-04-08 23:35:44 +0530

    started app with redux setup

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './global/redux/store'
+import Home from './pages/home/home'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Home,
+  },
+])
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+)
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons'
+import { Button, Layout, Menu, Space, theme } from 'antd'
+import { decrement, increment } from '../../global/redux/counter/couter-slice'
+import { useAppSelector, useAppDispatch } from '../../global/redux/hooks'
+
+const { Header, Sider, Content } = Layout
+
+export default function Home() {
+  const [collapsed, setCollapsed] = useState(false)
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken()
+
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className='demo-logo-vertical' />
+        <Menu
+          theme='dark'
+          mode='inline'
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type='text'
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <div>
+            <Space>
+              <Button onClick={() => dispatch(increment())}>Increment</Button>
+
+              <Button onClick={() => dispatch(decrement())}>Decrement</Button>
+              <span>{count}</span>
+            </Space>
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  )
+}
